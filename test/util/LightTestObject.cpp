5fda07f803e7cb86da06e46b217973552f4c1059
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2016-09-16 08:16:28 +0200

    Forgot to init static fields in cpp file

--- test/util/LightTestObject.cpp
+++ test/util/LightTestObject.cpp
@@ -1,5 +1,10 @@
 #include "LightTestObject.h"
 
+int LightTestObject::nbTimeConstructorCalled = 0;
+int LightTestObject::nbTimeCopyConstructorCalled = 0;
+int LightTestObject::nbTimeMoveConstructorCalled = 0;
+int LightTestObject::nbTimeDestructorCalled = 0;
+
 LightTestObject::LightTestObject(int value) :
 	value(value) {
 	nbTimeConstructorCalled ++;
