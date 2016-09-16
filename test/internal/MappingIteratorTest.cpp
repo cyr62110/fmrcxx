ce0610762ce2a6ae6e8858ce8cbd8c2ccae82aa0
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2016-09-16 08:17:01 +0200

    Better test by mapping to string

--- test/internal/MappingIteratorTest.cpp
+++ test/internal/MappingIteratorTest.cpp
@@ -11,10 +11,10 @@ using namespace fmrcxx::internal;
 
 TEST_CASE( "check doComputeNext in static MappingIterator", "[MappingIterator]" ) {
 	Range<int> range1(1, 5);
-	MappingIterator<int, int, Range<int>, false> it([](int& elt) {
-		return elt * 2;
+	MappingIterator<std::string, int, Range<int>, false> it([](int& elt) {
+		return std::to_string(elt);
 	}, std::move(range1));
-	REQUIRE( *(it.doComputeNext()) == 2 );
+	REQUIRE( *(it.doComputeNext()) == "1" );
 }
 
 TEST_CASE ("check number of times constructor and destructor are called in static MappingIterator", "[MappingIterator]") {
@@ -42,10 +42,10 @@ TEST_CASE ("check doComputeNext in static MappingIterator uses copy iterator if 
 
 TEST_CASE( "check doComputeNext in dynamic MappingIterator", "[MappingIterator]" ) {
 	Range<int> range1(1, 5);
-	MappingIterator<int, int, Range<int>, true> it([](int& elt) {
-		return new int(elt * 2);
+	MappingIterator<std::string, int, Range<int>, true> it([](int& elt) {
+		return new std::string(std::to_string(elt));
 	}, std::move(range1));
-	REQUIRE( *(it.doComputeNext()) == 2 );
+	REQUIRE( *(it.doComputeNext()) == "1" );
 }
 
 TEST_CASE ("check number of times constructor and destructor are called in dynamic MappingIterator", "[MappingIterator]") {
