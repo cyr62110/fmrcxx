de40d41a1c7cb5252105a5cd37c591d1630dd082
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2016-09-20 23:29:12 +0200

    Library is for C++11 instead of 14

--- test/IteratorTest.cpp
+++ test/IteratorTest.cpp
@@ -8,9 +8,9 @@ using namespace fmrcxx;
 TEST_CASE( "Can chain iterator operation", "[Iterator]" ) {
 	Range<int> range(1, 5);
 
-	const auto& it = range.filter([](auto value) {
+	const auto& it = range.filter([](const int& value) {
 			return (value & 1) == 0;
-		}).filter([](auto value) {
+		}).filter([](const int& value) {
 			return (value % 2) == 0;
 		});
 }
